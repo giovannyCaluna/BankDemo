08592b7c6451dc85ff9dea72df9d8c25fef7e602
giovannyCaluna <gio-vanny1@hotmail.com>
2024-07-26 17:32:11 +0200

    Remove unnecesary code

--- src/main/java/com/example/demo/repository/ClientRepository.java
+++ src/main/java/com/example/demo/repository/ClientRepository.java
@@ -1,10 +1,7 @@
 package com.example.demo.repository;
 
 import com.example.demo.model.Client;
-import com.example.demo.model.Tutorial;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
 
 
 public interface ClientRepository extends JpaRepository<Client, Long> {
